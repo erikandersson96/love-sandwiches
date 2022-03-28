54ac6b11e6f2c283f5f6d3d3cfe6e76957fc5f5c
erikandersson96 <erik96.andersson@gmail.com>
2022-03-28 10:01:20 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+# IMPORTANT! Use: pip3 freeze > requirements.txt in terminal to add to your txt file before heroku deployment
+
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,6 +18,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+# Remember to allways add \n (new line) to input methods before deployment 
 def get_sales_data(): 
     """
     Get sales figures input from the user. 
@@ -28,7 +31,7 @@ def get_sales_data():
         print("Data should be six numbers, seperated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
          
